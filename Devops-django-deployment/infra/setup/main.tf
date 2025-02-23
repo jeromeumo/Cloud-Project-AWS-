4e8281bf24851de3557ecf4ebe0a90a28de4c76e
Jeromeumo <jeromeumo@gmail.com>
2025-02-23 15:40:25 +0000

    added terraform

---
+++ Devops-django-deployment/infra/setup/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-app-set"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devop-app-lock"
+
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+
+}
+
+
